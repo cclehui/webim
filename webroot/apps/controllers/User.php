36832e68f21157bf57368aa86c47e135ce79b3df
cclehui <cclehui@sina.com>
2018-01-14 20:14:15 +0800

    add tpl

--- webroot/apps/controllers/User.php
+++ webroot/apps/controllers/User.php
@@ -17,6 +17,18 @@ class User extends \Swoole\Controller {
 
     function login_ajax() {
 
+        $user_name = $this->request->post['name'];
+        $password = $this->request->post['password'];
+
+        if (!$user_name) {
+            echo $this->message(100, '用户名不能为空');
+            die;
+        }
+
+        $this->session->start();
+        $_SESSION['isLogin'] = true;
+        $_SESSION['user'] = md5($user_name);
+
         $result = [
             'time' => time()
 
---
+++ webroot/apps/models/CommonUtil.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/13
+ * Time: 23:03
+ */
+
+namespace App\Models;
+
+class CommonUtil
+{
+
+}
